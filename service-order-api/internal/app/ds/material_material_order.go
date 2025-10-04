703625baa39405216c7ac58e70e3d8e4cb27000c
Mopolop <knyazevam@student.bmstu.ru>
2025-10-04 15:14:02 +0300

    style additing

--- service-order-api/internal/app/ds/material_material_order.go
+++ service-order-api/internal/app/ds/material_material_order.go
@@ -4,10 +4,11 @@ import "database/sql"
 
 // MaterialMaterialOrder представляет связь "материал ↔ заказ" (многие ко многим)
 type MaterialMaterialOrder struct {
-	ID              int `gorm:"primaryKey;autoIncrement"`
-	MaterialID      int `gorm:"not null"`
-	MaterialOrderID int `gorm:"not null"`
-	WallLength      sql.NullFloat64
+	ID                   int `gorm:"primaryKey;autoIncrement"`
+	MaterialID           int `gorm:"not null"`
+	MaterialOrderID      int `gorm:"not null"`
+	WallLength           sql.NullFloat64
+	MaterialsConsumption sql.NullFloat64
 
 	Material      Material      `gorm:"foreignKey:MaterialID;references:ID"`
 	MaterialOrder MaterialOrder `gorm:"foreignKey:MaterialOrderID;references:ID"`
